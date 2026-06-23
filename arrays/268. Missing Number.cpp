99c92f80a9c00e01b4f39b4f4166fdfd5563c357
shaauryaa <shauryanigam2006@gmail.com>
2026-06-23 22:37:31 +0530

    solve: Missing Number (Easy) - Arrays

---
+++ arrays/268. Missing Number.cpp
@@ -0,0 +1,46 @@
+// Problem  : 268. Missing Number
+// Link     : https://leetcode.com/problems/missing-number/
+// Difficulty: Easy
+// Date     : 23/06/2026
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+/*Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+Output: 2
+Explanation:
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation:
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.*/
+
+class Solution {
+   public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result = n;  // initialize result with n, as the missing number could be n itself
+        for (int i = 0; i < n; i++) {
+            result ^= i ^ nums[i];  // XOR all indices and numbers together. The missing number will be the only one left after all pairs cancel out.
+        }
+        return result;
+    }
+};
+
+// Optimal Complexity
+//   Time  : O(n)
+//   Space : O(1)
