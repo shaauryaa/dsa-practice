1c58f58d22337659928529bfcd6c2f9022f9db83
shaauryaa <shauryanigam2006@gmail.com>
2026-07-25 23:55:33 +0530

    solve: Binary Search (Easy) - Binary Search

---
+++ binary-search/704. Binary Search.cpp
@@ -0,0 +1,46 @@
+// Problem  : 704. Binary Search
+// Link     : https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+// Date     : 24/07/2026
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+/*Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1*/
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+
+// Iterative Approach Complexity
+//   Time  : O(log(n))
+//   Space : O(1)
