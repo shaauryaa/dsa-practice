f3e894c8eac3239cbd6ad0e04ea5ab45faaa0fff
shaauryaa <shauryanigam2006@gmail.com>
2026-07-28 00:07:23 +0530

    solve: Lower and Upper Bound (Easy) - Binary Search

--- binary-search/Lower And Upper Bound.cpp
+++ binary-search/Lower And Upper Bound.cpp
@@ -40,3 +40,40 @@ class Solution {
 
 // time compexity = O(logn)
 // space compexity = O(1)
+
+// upper bound
+
+/*Example 1
+Input : n= 4, nums = [1,2,2,3], x = 2
+Output:3
+Explanation:
+Index 3 is the smallest index such that arr[3] > x.
+
+Example 2
+Input : n = 5, nums = [3,5,8,15,19], x = 9
+Output: 3
+Explanation:
+Index 3 is the smallest index such that arr[3] > x.*/
+
+class Solution {
+   public:
+    int upperBound(vector<int>& nums, int x) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        int answer = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > x) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return answer;
+    }
+};
+
+// time compexity = O(logn)
+// space compexity = O(1)
