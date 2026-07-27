87fb984f46d2d8de965fe2fafe12fc45b57d02f9
shaauryaa <shauryanigam2006@gmail.com>
2026-07-28 00:53:46 +0530

    solve: Search Insert Position (Easy) - Binary Search

--- binary-search/35. Search Insert Position.cpp
+++ binary-search/35. Search Insert Position.cpp
@@ -1,7 +1,54 @@
-// Problem  : Lower Bound
-// Link     : https://takeuforward.org/plus/dsa/problems/lower-bound-?source=strivers-a2z-dsa-track
+// Problem  : 35. Search Insert Position
+// Link     : https://leetcode.com/problems/search-insert-position/description/
 // Difficulty: Easy
 // Date     : 27/07/2026
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+/*Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4*/
+
+// same code as the lower bound code logic
+class Solution {
+   public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        int answer = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return answer;
+    }
+};
+
+// time compexity = O(logn)
+// space compexity = O(1)
+
+// Problem  : Lower Bound
+// Link     : https://takeuforward.org/plus/dsa/problems/lower-bound-?source=strivers-a2z-dsa-track
 
 /*Example 1
 Input : nums= [1,2,2,3], x = 2
